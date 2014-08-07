07c23411040274ac7eca15ffb34314387dd3e5a3
Whitney Williams <wweliz@gmail.com>
2014-08-07 11:49:44 -0400

    fixing misspellings; adding hover text for portfolio page

--- app/scripts/workview.js
+++ app/scripts/workview.js
@@ -30,23 +30,23 @@ var WorkView = Backbone.View.extend({
 	},
 
 	showMiceSummary: function(){
-		$('.project-text').html('Short description about mice.');
+		$('.project-text').html('Mice On Main is a webapp that utilizes the HTML5 geolocation API to help visitors to Greenville's downtown find the mice sculptures hidden along Main Street.');
 	},
 
 	showGithubSummary: function(){
-		$('.project-text').html('Short description about github.');
+		$('.project-text').html('I rebuilt my GitHub profile page as a way to practice structuring my HTML, styling with CSS, and pulling data from public APIs.');
 	},
 
 	showDiceSummary: function(){
-		$('.project-text').html('Short description about dice.');
+		$('.project-text').html('As part of a team project at the Iron Yard, we created a vitual dice roller that allows you to choose both the number of dice and the number of sides on each die.');
 	},
 
 	showEskimoSummary: function(){
-		$('.project-text').html('Short description about eskimo.');
+		$('.project-text').html('The ETB is the only theatre in Berlin that presents plays exclusively in English; I illustrated this poster for the play Prophecy of a Nameless Eskimo.');
 	},
 
 	showChomaratSummary: function(){
-		$('.project-text').html('Short description about chomarat.');
+		$('.project-text').html('The French textile company Chomarat needed help applying the brand identity created in France to the .');
 	},
 
 	showHoowakiSummary: function(){
@@ -63,10 +63,6 @@ var WorkView = Backbone.View.extend({
 
 	showScanSourceSummary: function(){
 		$('.project-text').html('Short description about scansource.');
-	},	
-
-	showProjectSummary: function(projectText){
-		$('.project-text').html(projectText);
 	}
 
 });
